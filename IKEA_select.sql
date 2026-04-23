b24aa376ae298638c9013c288c53bc0b2b95a486
alexruden4 <alexruden4@gmail.com>
2026-04-23 23:15:51 +0200

    Create IKEA_select.sql

---
+++ IKEA_select.sql
@@ -0,0 +1,21 @@
+"FULL INFO ABOUT THE ORDERING"
+SELECT c.first_name, c.last_name, p.product_name, oi.quantity, o.order_date
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id;#
+
+"TOTAL AMOUNT OF CUSTOMER ORDERS"
+SELECT c.first_name, c.last_name, SUM(oi.quantity * p.price) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY c.customer_id;
+
+"THE MOST BOUGHT PRODUCT"
+SELECT p.product_name, SUM(oi.quantity) AS total_sold
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY p.product_id
+ORDER BY total_sold DESC;
